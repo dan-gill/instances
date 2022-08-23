6365596a050825e944d177849725f94d77466ad1
Dan Gill <dgill@gocloudwave.com>
2022-08-23 20:42:43 +0000

    vm created

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state" "network_details" {
     region = "us-east-1"
   }
 }
+
+resource "aws_instance" "my_vm" {
+  ami = "ami-08d4ac5b634553e16"
+  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+  instance_type = "t3.micro"
+  tags = {
+    Name = "student.2-vm1"
+  }
+}
