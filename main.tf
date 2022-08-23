d5728a67ff2e673b09a07d1ae003da2f9f8b87bd
Dan Gill <dgill@gocloudwave.com>
2022-08-23 21:38:54 +0000

    update SG for vm

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ resource "aws_instance" "my_vm" {
   ami = "ami-08d4ac5b634553e16"
   subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
   instance_type = "t3.micro"
-  tags = {
   key_name = data.terraform_remote_state.network_details.outputs.key_name
+  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
+  tags = {
     Name = "student.2-vm1"
   }
 }
