41ff0f82a5508835bf8b30203843a95024440720
Dan Gill <dgill@gocloudwave.com>
2022-08-24 16:15:59 +0000

    End Of Lab: Reusable Code - Modules

--- main.tf
+++ main.tf
@@ -7,11 +7,12 @@ data "terraform_remote_state" "network_details" {
   }
 }
 
-resource "aws_instance" "my_vm" {
+module "webserver" {
+  source = "./modules/linux_node"
   ami = "ami-08d4ac5b634553e16"
-  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = data.terraform_remote_state.network_details.outputs.key_name
+  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
   vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
   tags = {
     Name = "student.2-vm1"
